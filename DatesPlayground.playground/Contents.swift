1f48bc35a7112ca7c0736e42df3612d4112f63c9
Amelia Brown <abrown@Roberts-Mac-mini.local>
2019-10-31 21:20:10 -0500

    Not as good as I'd like

--- DatesPlayground.playground/Contents.swift
+++ DatesPlayground.playground/Contents.swift
@@ -4,13 +4,24 @@ import Foundation
 
 let date = Date()
 
+
 // Create a date using today's date and a DateComponents structure
+
+let calendar = Calendar.current
+
+let dateComponents = calendar.dateComponents([Calendar.Component.day, Calendar.Component.month, Calendar.Component.year], from: date)
 
 // Compare the two dates to see if they occur in the same Calendar day
 
-// Use a date formatter
+let compDate = date as NSDate
+
+if compDate.isEqual(to: dateComponents.date)  {
+    print("Same day!")
+}
 
 let formatter = DateFormatter()
 
 // use the formatter to print the day in a friendly format.
 // hint, use NSDateFormatter.com to get an appropriate printing format string.
+formatter.locale = Locale(identifier: "en_US")
+print(formatter.string(from: date))
